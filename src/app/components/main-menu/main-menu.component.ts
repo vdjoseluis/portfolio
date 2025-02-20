ba77643c2e6325d9919e36663e68be3a7035a1ad
Jose Vasquez <vdjoseluis@outlook.com>
2025-02-20 17:17:23 +0100

    Create about-page

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -6,10 +6,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-main-menu',
   standalone: true,
-  imports: [MatDividerModule, RouterLink],
+  imports: [MatDividerModule, RouterLink, RouterLinkActive],
   templateUrl: './main-menu.component.html',
   styles: ``
 })
 export default class MainMenuComponent {
-
+  menuItems = [
+    { key: 'about', value: 'Acerca de mi' },
+    { key: 'skills', value: 'Tecnologías' },
+    { key: 'projects', value: 'Proyectos' },
+    { key: 'contact', value: 'Contacto' }
+  ]
 }
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild, viewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatTooltipModule],
   templateUrl: './about.component.html',
-  styles: ``
+  styleUrls: ['./about.component.css']
 })
-export default class AboutComponent {
+export default class AboutComponent implements AfterViewInit{
+  @ViewChild('paragraphsContainer') paragraphsContainer!: ElementRef;
+  constructor(private renderer: Renderer2) { }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.paragraphsContainer) {
+        this.renderer.removeClass(this.paragraphsContainer.nativeElement, 'opacity-0');
+      }
+    }, 1000);
+  }
 
 }
