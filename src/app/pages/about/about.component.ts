7dd86b01fe4db447a771ae04d859736489944b6e
Jose Vasquez <vdjoseluis@outlook.com>
2025-02-28 21:56:37 +0100

    Create projects and contact page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink, MatTooltipModule, TranslateModule, CommonModule],
+  imports: [RouterLink, MatTooltipModule, TranslateModule, CommonModule, MatIconModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatTabsModule} from '@angular/material/tabs';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule, MatTooltipModule, MatTabsModule, MatIconModule],
   templateUrl: './projects.component.html',
   styles: ``
 })
 export default class ProjectsComponent {
+  activeTabIndex = 0;
 
+  onTabChange(index: number) {
+    this.activeTabIndex = index;
+  }
 }
